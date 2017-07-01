bab0da42b70e6313135a8c279128e8e827c33d09
Socheat <socheatsok78@gmail.com>
2017-07-01 13:20:47 +0700

    Populate, Translations Model with existing code
    Grab from my other project

--- src/Translations/Translations.php
+++ src/Translations/Translations.php
@@ -11,8 +11,25 @@ namespace Wpml\Translations;
 class Translations extends Model
 {
 
-  function __construct()
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $connection = 'wordpress';
+  protected $table = 'icl_translations';
+  protected $primaryKey = 'translation_id';
+
+  /**
+   * Override newCollection() to return a custom collection.
+   *
+   * @param array $models
+   *
+   * @return \WPML\TranslationsCollection
+   */
+  public function newCollection(array $models = [])
   {
-    //
+    return new TranslationsCollection($models);
   }
+
 }
