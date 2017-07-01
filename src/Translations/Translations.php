b3e25d9ea17e9a3c502823a15284b96350d6b83c
Socheat <socheatsok78@gmail.com>
2017-07-01 13:18:47 +0700

    Add Translations/Translations Model

---
+++ src/Translations/Translations.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *  Translations Model
+ *
+ * @author Socheat <https://github.com/socheatsok78>
+ */
+
+namespace Wpml\Translations;
+
+class Translations extends Model
+{
+
+  function __construct()
+  {
+    //
+  }
+}
