848000e87cd07baebb94f80aa32f45afb2008cdf
Socheat <socheatsok78@gmail.com>
2017-07-01 13:18:34 +0700

    Add Post Model

---
+++ src/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *  Post Model
+ *
+ * @author Socheat <https://github.com/socheatsok78>
+ */
+
+namespace Wpml;
+
+use Corcel\Post as Corcel;
+
+class Post extends Corcel
+{
+
+  function __construct()
+  {
+    //
+  }
+}
