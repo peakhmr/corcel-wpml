14d548ee48b2db18afd1e503f95095343817b8dd
Socheat <socheatsok78@gmail.com>
2017-07-01 12:13:10 +0700

    Add Empty ServiceProvider

---
+++ src/laravel/SitepressServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Socheatsok78\Wpml;
+
+use Illuminate\Support\ServiceProvider;
+
+class SitepressServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+
+
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/laravel/config.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    // Set the connection name you'd like to use with Corcel models and classes
+    'connection' => 'corcel',
+];
