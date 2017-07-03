be6820656efb542116e67185f9f49d93d4816e66
Socheat <socheatsok78@gmail.com>
2017-07-03 14:16:46 +0700

    Add Wpml\PostBuilder.php

---
+++ src/PostBuilder.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *  PostBuilder for Post
+ *
+ * @author Socheat <https://github.com/socheatsok78>
+ */
+
+namespace Wpml;
+
+use Corcel\PostBuilder as Builder;
+use Wpml\Post;
+use Wpml\Translation\Translation;
+
+class PostBuilder extends Builder
+{
+
+
+
+}
